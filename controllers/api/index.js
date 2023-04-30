4e15b1cec0449d5115ebbea1e56a9e7ff8097e80
goldie <codegayle@gmail.com>
2023-04-30 15:31:25 -0400

    get routes connected. still testing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const animalRoutes = require('./AnimalRoutes');
-//const breedRoutes = require('./breedRoutes');
+const breedRoutes = require('./breedRoutes');
+const tagRoutes = require('./tagRoutes')
+const typeRoutes = require('./typeRoutes')
+//const userRoutes = require('./userRoutes')
 
 router.use('/animals', animalRoutes);
-//router.use('/breeds', breedRoutes);
+router.use('/breeds', breedRoutes);
+router.use('/tags', tagRoutes);
+router.use('/type', typeRoutes);
+//router.use('/user' userRoutes)
 
 module.exports = router;
