fae942e5b248e35a89b34eb8d2b94c7df95140a1
rtolefree1 <r_ryde2000@yahoo.com>
2023-05-01 18:29:17 -0400

    adding some CRUD operations

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,12 @@ const animalRoutes = require('./AnimalRoutes');
 const breedRoutes = require('./breedRoutes');
 const tagRoutes = require('./tagRoutes')
 const typeRoutes = require('./typeRoutes')
-//const userRoutes = require('./userRoutes')
+const userRoutes = require('./userRoutes')
 
 router.use('/animals', animalRoutes);
 router.use('/breeds', breedRoutes);
 router.use('/tags', tagRoutes);
 router.use('/type', typeRoutes);
-//router.use('/user' userRoutes)
+router.use('/users', userRoutes);
 
 module.exports = router;
