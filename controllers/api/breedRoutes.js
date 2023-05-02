4b6b2e62f1499a9f4c54d1a484f65893c23e92df
goldie <codegayle@gmail.com>
2023-05-02 07:03:40 -0400

    added get routes and handlebars connection for animal/breeds/tags

--- controllers/api/breedRoutes.js
+++ controllers/api/breedRoutes.js
@@ -2,33 +2,5 @@ const router = require('express').Router();
 const { Animal, Breed,  Type} = require('../../models');
 
 
-router.get('/', async (req, res) => {
-    try{
-        const breedData = await Breed.findAll({
-            include:
-              [{ model: Breed },  {model: Type}]
-          });
-        res.status(200).json(breedData)
-    }catch(err){
-        res.status(400).json(err)
-    }
-})
-
-router.get('/:id', async (req, res) => {
-    try {
-      const breedData = await Breed.findByPk(req.params.id, {
-  
-        include:
-          [{ model: Breed },  {model:Type}, {model: Animal}]
-      });
-      if (!breedData) {
-        res.status(404).json({ message: `No ID found for ${req.params.id}` })
-      }
-      res.status(200).json(breedData)
-    } catch (err) {
-      res.status(500).json(err)
-    }
-  
-  });
 
   module.exports = router;
--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Animal, Breed, Tags, Type, User} = require('../models');
+const { Animal, Breed, Tags, Type, User } = require('../models');
 //const withAuth = require('../utils/auth');
 
 
@@ -15,59 +15,169 @@ const { Animal, Breed, Tags, Type, User} = require('../models');
 })*/
 
 router.get('/', async (req, res) => {
-   try{
-       const animaldata = await Animal.findAll({
-           include: [
-           {
-            model: Type,
-            attributes: ['type'],
-          },
-          {
-            model:Breed,
-            attributes: ['breed']
-          }]
-             
-         });
-      
-         const animals= animaldata.map((animal) => animal.get({ plain: true }));
+   try {
+      const animaldata = await Animal.findAll({
+         include: [
+            {
+               model: Type,
+               attributes: ['type'],
+            },
+            {
+               model: Breed,
+               attributes: ['breed']
+            }]
 
-         
-        res.render('homepageg', { 
-            animals, 
-            logged_in: req.session.logged_in 
-          });
-        } catch(err){
-       res.status(400).json(err)
+      });
+
+      const animals = animaldata.map((animal) => animal.get({ plain: true }));
+
+
+      res.render('homepageg', {
+         animals,
+         logged_in: req.session.logged_in
+      });
+   } catch (err) {
+      res.status(400).json(err)
    }
 })
 
 router.get('/animal/:id', async (req, res) => {
    try {
-     const animalData = await Animal.findByPk(req.params.id, {
+      const animalData = await Animal.findByPk(req.params.id, {
+
+         include:
+            [{
+               model: Type,
+               attribute: ['type']
+            },
+            {
+               model: Breed,
+               attribute: ['breed']
+            },
+            {
+               model: Tags,
+               attribute: ['tag_name']
+            }]
+      });
+
+
+
+      const animal = animalData.get({ plain: true });
+      res.render('oneanimal', {
+         ...animal,
+         logged_in: req.session.logged_in
+      });
+      if (!animalData)
+         res.render('invalidroute')
+
+   } catch (err) {
+      res.status(500).json(err)
+   }
+
+});
+
+
+
+router.get('/breed/:id', async (req, res) => {
+   try {
+      const breedData = await Breed.findByPk(req.params.id, {
+
+         include:
+            [{
+               model: Animal,
+               attribute: ['name']
+            }]
+      });
+
+      const breedname = breedData.breed;
+      const breedanimals = breedData.animals.map((breedanimal) => breedanimal.get({ plain: true }));
+
+
+      res.render('breeds', {
+         breedname,
+         breedanimals,
+         logged_in: req.session.logged_in
+      });
+
+
+
+   } catch (err) {
+      res.status(500).json(err)
+   }
+
+});
+
+router.get('/tags/:id', async (req, res) => {
+   try {
+   
+      const tagdata = await Tags.findByPk(req.params.id, {
+
+         include:
+            [{
+               model: Animal,
+               attribute: ['name']
+            }]
+      });
+
+      
+      // const taganimals = tagdata.animals.map((taganimal) => taganimal.get({ plain: true }));
+      const tagname = tagdata.tag_name;
+      const taganimals = tagdata.animals.map((taganimal) => taganimal.get({ plain: true }))
+
+
+      res.render('tags', {
+         tagname,
+         taganimals,
+         logged_in: req.session.logged_in
+      });
+
+   } catch (err) {
+      res.status(500).json(err)
+   }
+
+});
+
+/*router.get('/tags', async (req, res) => {
+    try{
+        const tagdata = await Tags.findAll({
+            include:
+              [{ model: Animal }]
+          });
+        res.status(200).json(tagdata)
+    }catch(err){
+        res.status(400).json(err)
+    }
+})
+
+
+  
+  });router.get('/types', async (req, res) => {
+   try{
+       const typedata = await Type.findAll({
+           include:
+             [{ model: Animal}, {model:Breed}]
+         });
+       res.status(200).json(typedata)
+   }catch(err){
+       res.status(400).json(err)
+   }
+})
+
+router.get('type/:id', async (req, res) => {
+   try {
+     const typeData = await Type.findByPk(req.params.id, {
  
        include:
-         [{model:Type,
-         attribute: ['type']},
-         { model: Breed, 
-         attribute:['breed']},
-         { model: Tags,
-         attribute: ['tag_name'] }]
+         [{ model: Breed }, { model: Animal}]
      });
-     
-     
-     
-     const animal = animalData.get({ plain: true });
-     res.render('oneanimal',{ 
-      ... animal, 
-      logged_in: req.session.logged_in 
-    });
-    if (!animalData) 
-       res.render('invalidroute')
-     
+     if (!typeData) {
+       res.status(404).json({ message: `No ID found for ${req.params.id}` })
+     }
+     res.status(200).json(typeData)
    } catch (err) {
      res.status(500).json(err)
    }
  
- });
+ });*/
 
 module.exports = router;
