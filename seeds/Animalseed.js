41eb66420a896f07ad4c1776282130841ac76c2a
goldie <codegayle@gmail.com>
2023-05-02 20:39:38 -0400

    added picUrl to model and routes

--- seeds/Animalseed.js
+++ seeds/Animalseed.js
@@ -6,6 +6,7 @@ const animalData = [
         typeid: 1,
         breedid: 2,
         adoptdate:2024-06-21,
+        picUrl:"https://public.oed.com/wp-content/uploads/MicrosoftTeams-image-1-e1663849645910-1130x400.jpg"
         
     },
     {
@@ -13,6 +14,7 @@ const animalData = [
         typeid: 1,
         breedid: 1,
         adoptdate:2023-06-21,
+        picUrl:"https://static.nationalgeographic.co.uk/files/styles/image_3200/public/01-chocolate-lab-nationalgeographic_1154144.webp?w=768&h=432"
         
     },
     {
@@ -20,6 +22,7 @@ const animalData = [
         typeid: 2,
         breedid: 3,
         adoptdate:2023-08-21,
+        picUrl: "https://cdn.mos.cms.futurecdn.net/3edtiu5BmNmLSyrgiWrdAM-1200-80.jpg.webp"
         
     },
     {
@@ -27,6 +30,7 @@ const animalData = [
         typeid: 2,
         breedid: 4,
         adoptdate:2023-09-30,
+        picUrl: "https://geniusvets.s3.amazonaws.com/gv-cat-breeds/Mixed-Breed-Cat-2.jpg"
     
     },
 ]
