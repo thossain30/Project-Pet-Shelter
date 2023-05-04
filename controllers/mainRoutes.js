f7208e60faf9177e6151a2dcc0fc0e43194047b7
goldie <codegayle@gmail.com>
2023-05-03 21:54:35 -0400

    added drop down options to main page

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -3,41 +3,90 @@ const { Animal, Breed, Tags, Type, User } = require('../models');
 //const withAuth = require('../utils/auth');
 
 
-router.get('/login', async (req, res) => {
+// router.get('/layout/main', async (req, res) =>{
+//    try{
+//       const typeData = await Type.findAll();
+//       const breedData = await Breed.findAll();
+//       const tagData = await Tags.findAll();
+//       const types = typeData.map((type) => type.get({plain:true}));
+//       const breeds =breedData.map((breed)=> breed.get({plain:true}));
+//       const tags =tagData.map((tag)=> tag.get({plain:true}))
+//       console.log(tags),
+//       res.render('', {
+//          tags,
+//          breeds,
+//          types,
+//          animals,
+//          logged_in: req.session.logged_in
+//       });
+      
+//    }catch(err){
+//       res.status(500).json(err)
+//    }
+
+// })
+router.get('/addpet', async (req, res) =>{
    try{
-      console.log('rending res object',res);
-        res.render('submitpage');
-       // res.status(200).json();
-   }catch(err)
-   {
-    res.status(500).json(err)
+   const breedData = await Breed.findAll();
+      
+      const breeds = breedData.map((breed) => breed.get({plain:true}));
+     
+
+      console.log(breeds)
+
+      res.render('addpet', {
+         
+         breeds,
+         
+         logged_in: req.session.logged_in
+      });
+   } catch (err) {
+      res.status(500).json(err)
+   }}
+)
+router.get('/login', async (req, res) => {
+   try {
+      console.log('rending res object', res);
+      res.render('submitpage');
+      // res.status(200).json();
+   } catch (err) {
+      res.status(500).json(err)
    }
 })
 router.get('/Adam', async (req, res) => {
-      try {
-         const animaldata = await Animal.findAll({
-            include: [
-               {
-                  model: Type,
-                  attributes: ['type'],
-               },
-               {
-                  model: Breed,
-                  attributes: ['breed']
-               }]
-   
-         });
-         
-         const animals = animaldata.map((animal) => animal.get({ plain: true }));
-   
-   
-         res.render('allpets', {
-            animals,
-            logged_in: req.session.logged_in
-         });
-   }catch(err)
-   {
-    res.status(500).json(err)
+   try {
+      const animaldata = await Animal.findAll({
+         include: [
+            {
+               model: Type,
+               attributes: ['type'],
+            },
+            {
+               model: Breed,
+               attributes: ['breed']
+            }]
+
+      });
+     // For drop down menu
+      const typeData = await Type.findAll();
+      const breedData = await Breed.findAll();
+      const tagData = await Tags.findAll();
+      const types = typeData.map((type) => type.get({plain:true}));
+      const breeds =breedData.map((breed)=> breed.get({plain:true}));
+      const tags =tagData.map((tag)=> tag.get({plain:true}))
+      console.log(tags)
+
+      const animals = animaldata.map((animal) => animal.get({ plain: true }));
+
+      res.render('allpets', {
+         tags,
+         breeds,
+         types,
+         animals,
+         logged_in: req.session.logged_in
+      });
+   } catch (err) {
+      res.status(500).json(err)
    }
 })
 router.get('/', async (req, res) => {
@@ -136,7 +185,7 @@ router.get('/breed/:id', async (req, res) => {
 
 router.get('/tags/:id', async (req, res) => {
    try {
-   
+
       const tagdata = await Tags.findByPk(req.params.id, {
 
          include:
@@ -146,7 +195,7 @@ router.get('/tags/:id', async (req, res) => {
             }]
       });
 
-      
+
       // const taganimals = tagdata.animals.map((taganimal) => taganimal.get({ plain: true }));
       const tagname = tagdata.tag_name;
       const taganimals = tagdata.animals.map((taganimal) => taganimal.get({ plain: true }))
@@ -193,13 +242,13 @@ router.get('/tags/:id', async (req, res) => {
 
 router.get('/type/:id', async (req, res) => {
    try {
-     const typeData = await Type.findByPk(req.params.id, {
- 
-       include:
-         [{ model: Breed }, { model: Animal}]
-     });
-     console.log(typeData)
-     const typename = typeData.type;
+      const typeData = await Type.findByPk(req.params.id, {
+
+         include:
+            [{ model: Breed }, { model: Animal }]
+      });
+      console.log(typeData)
+      const typename = typeData.type;
       const typeanimals = typeData.animals.map((typeanimal) => typeanimal.get({ plain: true }))
 
 
@@ -214,5 +263,14 @@ router.get('/type/:id', async (req, res) => {
    }
 
 });
+
+router.get('/createtype', async (req, res) => {
+   try {
+      const type = await Type.findAll()
+
+   } catch (err) {
+      res.status(500).json(err)
+   }
+})
 
 module.exports = router;
