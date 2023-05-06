00853d267196568fbda8a575463d1fa40a47ae4d
rtolefree1 <r_ryde2000@yahoo.com>
2023-05-06 09:50:28 -0400

    Updated seeds

--- seeds/Animalseed.js
+++ seeds/Animalseed.js
@@ -37,6 +37,24 @@ const animalData = [
         desc: "Matilda was found behind Taqueria Del Sol"
     
     },
+    {
+        name: 'Floetry',
+        typeid: 3,
+        breedid: 3,
+        adoptdate:2023-07-21,
+        picUrl: "https://static.nationalgeographic.co.uk/files/styles/image_3200/public/3nationalgeographic_2758699.webp?w=1600&h=900",
+        desc: "Floetry description"
+    
+    },
+    {
+        name: 'Triples - Nick, Nicole and Nancy',
+        typeid: 3,
+        breedid: 3,
+        adoptdate:2023-08-14,
+        picUrl: "https://static.nationalgeographic.co.uk/files/styles/image_3200/public/poy1.webp?w=1600&h=900",
+        desc: "Nick and Nicole description"
+    
+    },
 ]
 
 const animalSeeds = () => Animal.bulkCreate(animalData);
